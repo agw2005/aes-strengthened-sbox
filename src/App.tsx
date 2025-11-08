98df6db1d84d4231fd0a0293257f79a786deb3a2
agw2005 <danial.agw2005@gmail.com>
2025-11-08 22:36:53 +0700

    feat(App.tsx): integrated the encryption process

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import { useState } from "react";
 import Header from "./components/Header.tsx";
-import { generateAes128Key } from "./math/aes128.ts";
+import {
+  blocksToString,
+  encryptBlock,
+  generateAes128Key,
+  stringToBlocks,
+} from "./math/aes128.ts";
 
 function App() {
-  const [plainText, setPlainText] = useState("");
   const [aesKey, setAesKey] = useState<Uint8Array>();
+  const [plainText, setPlainText] = useState<string>("");
+  const [encryptedPlainText, setEncryptedPlainText] = useState<string>("");
 
   const handleGenerateAesKey = () => {
     setAesKey(generateAes128Key());
+  };
+
+  const handleEncryptPlainText = () => {
+    try {
+      if (aesKey === undefined) {
+        throw new Error(`Key has not been generated yet`);
+      }
+    } catch (err) {
+      console.error(err);
+      return;
+    }
+    const plainTextBlock = stringToBlocks(plainText);
+    const encryptedTextBlock = encryptBlock(plainTextBlock, aesKey);
+    const encryptedTextString = blocksToString(encryptedTextBlock);
+    setEncryptedPlainText(encryptedTextString);
   };
 
   return (
@@ -18,14 +39,14 @@ function App() {
         <button
           onClick={handleGenerateAesKey}
           type="button"
-          className="bg-white hover:bg-gray-500 hover:text-white p-4"
+          className="h-max self-center rounded-2xl bg-white hover:bg-gray-500 hover:text-white active:bg-amber-300 active:text-black p-4"
         >
           Generate
         </button>
         <p className="text-white">{`AES Key : ${aesKey ? "" : "N/A"}`}</p>
         {aesKey
           ? (
-            <div className="border-2 border-white w-1/2 p-2">
+            <div className="border-2 border-white rounded-2xl min-w-max max-w-1/2 p-2">
               <p className="text-white break-all text-center">{aesKey}</p>
             </div>
           )
@@ -49,6 +70,13 @@ function App() {
             </textarea>
           </form>
         </div>
+        <button
+          type="button"
+          onClick={handleEncryptPlainText}
+          className="h-max self-center rounded-2xl bg-white hover:bg-gray-500 hover:text-white active:bg-amber-300 active:text-black p-4"
+        >
+          Encrypt
+        </button>
         <div className="flex-1 flex flex-col gap-2">
           <div className="flex-1">
             <h2 className="text-white text-center font-bold text-xl">
@@ -57,8 +85,8 @@ function App() {
           </div>
           <form action="" className="flex-99">
             <textarea
-              onChange={(e) => setPlainText(e.currentTarget.value)}
-              value={plainText}
+              disabled
+              value={encryptedPlainText}
               className="text-white border-white overflow-y-scroll p-2 text-sm resize-none border-5 rounded-2xl w-full h-full"
             >
             </textarea>
